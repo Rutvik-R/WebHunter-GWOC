e5e5539101377d23a91b58eb4f51d5b42f10c543
Rutvik-R <rutvikranpariya1221@gmail.com>
2023-01-13 16:03:35 +0530

    Home page

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,28 @@
+import Image from 'next/image'
+// import logo from '../public/logo.jpg'
+import {Satisfy} from "@next/font/google"
+
+const satisfy = Satisfy({
+  weight:'400',
+  subsets:['latin'],
+})
 export default function Home() {
   return (
-    <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
+    <div className='h-screen w-full space-y-7 bg-green-600'>
+    <div className='flex  justify-center space-x-10 pt-4'>
+      <a className='text-xl' href="">Home</a>
+      <a className='text-xl' href="">About</a>
+      <a className='text-xl' href="">Contact</a>
+    </div>
+    <div className= 'flex flex-col items-center justify-center  w-full space-y-2 '> 
+    <img
+      src="/logo.jpg"
+      width={500}
+      height={500}  
+      className='rounded-full'  
+  />
+  <p className={`text-6xl ${satisfy.className} text-yellow-400`}>That.gingerart</p>
+    </div>
+  </div>
   )
 }
