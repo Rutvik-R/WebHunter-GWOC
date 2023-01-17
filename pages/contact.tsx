a46e1da19273fb6d78b55a0138c311f392a5f979
Rutvik-R <rutvikranpariya1221@gmail.com>
2023-01-17 14:53:45 +0530

    contact us page

--- pages/contact.tsx
+++ pages/contact.tsx
@@ -1,7 +1,64 @@
-import React from "react";
+import React, { useRef } from 'react';
+import emailjs from '@emailjs/browser';
 
-function Contact() {
-  return <div>contact</div>;
+export default function App(){
+
+    const form = useRef<HTMLFormElement>(null);
+
+    const sendEmail = (e:any) => {
+      e.preventDefault();
+      
+      if(form.current == undefined) return;
+
+      emailjs.sendForm('service_7f2mzgh', 'template_alnojz6', form.current, 'KpUN9e-HpNZ65qr92')
+        .then((result) => {
+            console.log(result.text);
+        }, (error) => {
+            alert(error.text);
+            return;
+        });
+        // console.log(form.current.resend.checked);
+        if(form.current.resend.checked){
+            console.log(form.current.email_from.value)
+            emailjs.sendForm('service_7f2mzgh', 'template_u03j972', form.current, 'KpUN9e-HpNZ65qr92')
+            .then((result) => {
+                console.log(result.text);
+            }, (error) => {
+                alert(error.text);
+                return;
+            });
+
+        }
+        
+        alert('sended')
+        
+    };
+
+    return (
+        <div className="h-screen w-screen bg-red-500 bg-repeat">
+            <div className="absolute bg-amber-500 h-5/6 w-1/2 top-20 right-10 overflow-y-scroll ">
+                <div className="text-center my-2 text-6xl ">
+                    Contact Us
+                </div>
+                <div className="">
+                
+                    <form ref={form} onSubmit={sendEmail}>
+                        <input type="text" name="name" placeholder="Enter your name"         required  className="block h-10 mx-28 my-10 w-2/3" />
+                        <input type="email" name="email_from" placeholder="Enter your email"        required  className="block h-10 mx-28 my-10 w-2/3" />
+                        <textarea name="message" placeholder="Enter your message" required  className="block mx-28 my-10 w-2/3 h-32 max-h-32 overflow-y-scroll" />
+                        <input type="hidden" name="email_to" value='webhunters4@gmail.com' />
+                        <input type="checkbox" name="resend" className="ml-28 mr-5 inline my-3" /> Do you want copy of your message as email ?
+                        <input type="submit" value="Send" className="block text-3xl mx-28 my-10 w-2/3 text-cyan-800 font-semibold"/>
+                    </form>
+                </div>
+            </div>
+            <div className="absolute h-3/4 w-1/2 top-20 left-10 ">
+                <div className="bg-black h-2/6 w-5/12 absolute top-14  left-10    text-center ">   <a href=''   className='align-middle'><img src="" alt='instagram png' className='h-3/4' /> Instra</a> </div>
+                <div className="bg-black h-2/6 w-5/12 absolute top-14 right-10    text-center ">   <a href=''   className='align-middle'><img src="" alt='email png' className='h-3/4' /> Email</a> </div>
+                <div className="bg-black h-2/6 w-5/12 absolute bottom-14 left-10  text-center ">   <a href=''   className='align-middle'><img src="" alt='location jpg' className='h-3/4' /> Address</a> </div>
+                <div className="bg-black h-2/6 w-5/12 absolute bottom-14 right-10 text-center ">   <a href=''   className='align-middle'><img src="" alt='call sym' className='h-3/4' /> phone No</a> </div>
+            </div>
+        </div>
+    )
+
 }
-
-export default Contact;
