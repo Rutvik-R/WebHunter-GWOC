91f1b82acc11de0c6421bb9ef980ee39d342fdd7
kaushik2107-bit <kaushiksbhowmick@gmail.com>
2023-01-17 17:25:31 +0530

    Request Modal Done

--- components/wavy.tsx
+++ components/wavy.tsx
@@ -1,27 +1,30 @@
 import { Satisfy } from "@next/font/google";
 import Image from "next/image";
-const satisfy = Satisfy({weight: "400", subsets: ["latin"]})
+const satisfy = Satisfy({ weight: "400", subsets: ["latin"] });
 
-export default function Wavy()
-{
-
-    return(
-        <div className="h-screen overflow-hidden w-screen bg-white flex flex-col items-center">
-       
-            <section className="Wavy">
-                <div className="wave wave1"></div>
-                <div className="wave wave2"></div>
-                <div className="wave wave3"></div>
-                <div className="wave wave4"></div>
-            </section>
-            <div className="absolute top-64 rounded-full z-[10000]">
-                <Image alt="image" src={"/logo.jpg"} width={300} height={300} className="object-contain rounded-full" />
-            </div>
-            <p className={`${satisfy.className} text-yellow-400 text-6xl mt-10`}>that.GingerArt</p>
+export default function Wavy() {
+  return (
+    <div className="h-screen overflow-hidden w-screen bg-white flex flex-col items-center">
+      <section className="Wavy">
+        <div className="wave wave1"></div>
+        <div className="wave wave2"></div>
+        <div className="wave wave3"></div>
+        <div className="wave wave4"></div>
+      </section>
+      <div className="absolute top-[50%] -translate-y-[40%] z-[10000]">
+        <div className="rounded-full">
+          <Image
+            alt="image"
+            src={"/logo.jpg"}
+            width={300}
+            height={300}
+            className="object-contain rounded-full"
+          />
         </div>
-    )
+        <p className={`${satisfy.className} text-yellow-400 text-6xl mt-10`}>
+          that.GingerArt
+        </p>
+      </div>
+    </div>
+  );
 }
-
-{/* <div className="relative bg-white overflow-hidden"> */}
-                {/* <Image alt={"image"} src={"/logo.jpg"} width={400} height={400} className="rounded-md hidden" /> */}
-            {/* </div> */}
