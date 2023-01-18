bba71410b02647c62ea0941ea20e74f38187e722
kaushik2107-bit <kaushiksbhowmick@gmail.com>
2023-01-18 22:34:20 +0530

    changes

--- pages/gallery.tsx
+++ pages/gallery.tsx
@@ -3,82 +3,40 @@ import Header from "@/components/Header";
 import Menu from "@/components/Menu";
 import React from "react";
 import { useRecoilState } from "recoil";
-import styles from "../styles/Gallary.module.css";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
+import Slider from "react-slick";
+
 function Gallary() {
   const [showMenu, setShowMenu] = useRecoilState(menuState);
-  return (
-    <div className="flex flex-col bg-black">
-      <Header />
-      <div
-        className={`flex-grow flex max-md:flex-col max-md:items-center justify-center pb-16`}
-      >
-        <ul className={`max-md:m-auto md:ml-auto ${styles.slides}`}>
-          <li id="slide1" className={styles.slides_li}>
-            <img
-              src="/gwoc-gallary1.jpg"
-              alt=""
-              className={styles.slides_img}
-            />
-          </li>
-          <li id="slide2" className={styles.slides_li}>
-            <img
-              src="/gwoc-gallary2.jpg"
-              alt=""
-              className={styles.slides_img}
-            />
-          </li>
-          <li id="slide3" className={styles.slides_li}>
-            <img
-              src="/gwoc-gallary3.jpg"
-              alt=""
-              className={styles.slides_img}
-            />
-          </li>
-          <li id="slide4" className={styles.slides_li}>
-            <img
-              src="/gwoc-gallary4.jpg"
-              alt=""
-              className={styles.slides_img}
-            />
-          </li>
-          <li id="slide5" className={styles.slides_li}>
-            <img
-              src="/gwoc-gallary5.jpg"
-              alt=""
-              className={styles.slides_img}
-            />
-          </li>
-        </ul>
 
-        <ul
-          className={`md:flex md:flex-col md:leading-none flex ml-auto max-md:mt-6`}
-        >
-          <li className={styles.thumbnails_li}>
-            <a href="#slide1" className={styles.thumbnails_a}>
-              <img src="/gwoc-gallary1.jpg" className={styles.thumbnails_img} />
-            </a>
-          </li>
-          <li className={styles.thumbnails_li}>
-            <a href="#slide2" className={styles.thumbnails_a}>
-              <img src="/gwoc-gallary2.jpg" className={styles.thumbnails_img} />
-            </a>
-          </li>
-          <li className={styles.thumbnails_li}>
-            <a href="#slide3" className={styles.thumbnails_a}>
-              <img src="/gwoc-gallary3.jpg" className={styles.thumbnails_img} />
-            </a>
-          </li>
-          <li className={styles.thumbnails_li}>
-            <a href="#slide4" className={styles.thumbnails_a}>
-              <img src="/gwoc-gallary4.jpg" className={styles.thumbnails_img} />
-            </a>
-          </li>
-          <li className={styles.thumbnails_li}>
-            <a href="#slide5" className={styles.thumbnails_a}>
-              <img src="/gwoc-gallary5.jpg" className={styles.thumbnails_img} />
-            </a>
-          </li>
-        </ul>
+  const settings = {
+    dots: true,
+    infinite: true,
+    speed: 500,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+  };
+
+  return (
+    <div className="flex flex-col h-screen bg-blue-500">
+      <Header />
+      <div className="h-screen w-full border-2 border-red-500">
+        <Slider {...settings} className="w-full h-full">
+          <div
+            className="w-full h-full"
+            style={{ backgroundImage: "url(/gwoc-gallary1.jpg)" }}
+          ></div>
+          <div className="">
+            <img alt={"image1"} src={"/gwoc-gallary1.jpg"} className="" />
+          </div>
+          <div className="">
+            <img alt={"image1"} src={"/gwoc-gallary1.jpg"} className="" />
+          </div>
+          <div className="">
+            <img alt={"image1"} src={"/gwoc-gallary1.jpg"} className="" />
+          </div>
+        </Slider>
       </div>
       {showMenu && <Menu />}
     </div>
